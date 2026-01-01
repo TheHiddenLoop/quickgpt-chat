13cce7b7fcfd1bd1f3dbc8ac02cac3e26371e2d0
Angesh Chauhan <chauhanangesh60@gmail.com>
2026-01-01 11:34:33 +0530

    bud fuxed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -45,7 +45,7 @@ export const signin = async (req, res) => {
             return res.status(400).json({success:false, message:"All fields are required"});
         }
 
-        const user = await User.findOne({email});
+        let user = await User.findOne({email});
 
         if(!user){
             return res.status(404).json({success:false, message:"User not found."});
